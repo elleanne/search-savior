df0b3d9a9e08684b1b33d28813014a8cb5386d05
eliboss <eacrouther@gmail.com>
2021-02-12 11:10:35 -0500

    Minor edits for format

--- bst.js
+++ bst.js
@@ -356,7 +356,11 @@ var BST = function () {
          */
 		predecessor: function () {
 			return predecessorNode(root);
-		}
+		},
+
+        getRoot: function () {
+            return root.value;
+        }
 	};
 };
 
@@ -364,29 +368,29 @@ var BST = function () {
  * Tests
  */
 
-var ipTree = new BST();
-ipTree.insert(4, "test4");
-ipTree.insert(1, "test1");
-ipTree.insert(10, "test10");
-ipTree.insert(2, "test2");
-ipTree.insert(3, "test3");
-ipTree.insert(9, "test9");
-ipTree.insert(8, "test8");
-ipTree.insert(5, "test5");
-ipTree.insert(7, "test7");
-ipTree.insert(6, "test6");
+// var ipTree = new BST();
+// ipTree.insert(4, "test4");
+// ipTree.insert(1, "test1");
+// ipTree.insert(10, "test10");
+// ipTree.insert(2, "test2");
+// ipTree.insert(3, "test3");
+// ipTree.insert(9, "test9");
+// ipTree.insert(8, "test8");
+// ipTree.insert(5, "test5");
+// ipTree.insert(7, "test7");
+// ipTree.insert(6, "test6");
 
-ipTree.traverse(function (key, value) {
-    console.log("The value of " + key + " is " + value + ".");
-});
+// ipTree.traverse(function (key, value) {
+//     console.log("The value of " + key + " is " + value + ".");
+// });
 
-console.log("Searching for 3 results in: " + ipTree.search(3));
+// console.log("Searching for 3 results in: " + ipTree.search(3));
 
-console.log("Min is " + ipTree.min());
-console.log("Max is " + ipTree.max());
+// console.log("Min is " + ipTree.min());
+// console.log("Max is " + ipTree.max());
 
-console.log("The successor of root is: " + ipTree.successor());
-console.log("The predecessor of root is: " + ipTree.predecessor());
+// console.log("The successor of root is: " + ipTree.successor());
+// console.log("The predecessor of root is: " + ipTree.predecessor());
 
 /*
  * License:
