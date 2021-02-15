cb9a23ef26af3ab119498ccc8cbeefad4cae574b
eliboss <eacrouther@gmail.com>
2021-02-15 15:35:06 -0500

    Make action listener for toggle

--- background.js
+++ background.js
@@ -1,5 +1,27 @@
-chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab);
-    chrome.tabs.insertCSS(null, {file: './slider.css'});
-    chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log("ran foreground"));
+var tFBool = false;
+var once = 0;
+function turnOn() {
+  console.log("on");
+  chrome.tabs.onActivated.addListener((tab) => {
+    chrome.tabs.get(tab.tabId, (current_tab_info) => {
+      console.log(current_tab_info.url);
+    });
+  });
+}
+
+function doAmazingThings() {
+  //alert("YOU AM AMAZING!");
+  if (!tFBool) {
+    tFBool = true;
+  } else {
+    tFBool = false;
+  }
+  console.log(tFBool);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var on = document.getElementById("checkboxid");
+  if (on) {on.addEventListener("click", doAmazingThings);}
+
 });
+
