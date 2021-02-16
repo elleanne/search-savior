8790f77f53b48fd211f9cd747a41a0c2e266a2ed
eliboss <56413692+eliboss@users.noreply.github.com>
2021-02-15 23:48:43 -0500

    Make action listener for toggle (#13)
    Make action listener for toggle and connect popup.html to background.js file

--- background.js
+++ background.js
@@ -1,5 +1,38 @@
-chrome.tabs.onActivated.addListener(tab => {
-    console.log(tab);
-    chrome.tabs.insertCSS(null, {file: './slider.css'});
-    chrome.tabs.executeScript(null, {file: './foreground.js'}, () => console.log("ran foreground"));
+
+var tFBool = false;
+
+document.addEventListener("DOMContentLoaded", function () {
+  var on = document.getElementById("checkboxid");
+  if (on) {
+    on.addEventListener("click", function () {
+      var x = document.getElementById("onoff");
+      if (!tFBool) {
+        tFBool = true;
+        x.innerHTML = "on";
+        z = "on";
+      } else {
+        tFBool = false;
+
+        x.innerHTML = "off";
+        z = "off";
+      }
+      console.log(tFBool);
+    });
+  }
+});
+
+// need to fiugure out how to save the previos state
+chrome.tabs.onActivated.addListener((tab) => {
+  chrome.tabs.get(tab.tabId, (current_tab_info) => {
+    var z;
+    document.addEventListener("DOMContentLoaded", function () {
+      z = document.getElementById("onoff");
+    });
+
+    if (z) {
+      if (z.innerHTML === "on") {
+        console.log(current_tab_info.url);
+      }
+    }
+  });
 });
