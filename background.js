3c2810fd7a10cc4cd23942f7779dabbde5dfca57
sakshigupta265 <54631569+sakshigupta265@users.noreply.github.com>
2021-02-18 00:46:59 +0530

    Save Toggle Switch (#14)
    Use storage to save the state of the on/off toggle

--- background.js
+++ background.js
@@ -1,38 +1,22 @@
 
-var tFBool = false;
 
-document.addEventListener("DOMContentLoaded", function () {
-  var on = document.getElementById("checkboxid");
-  if (on) {
-    on.addEventListener("click", function () {
-      var x = document.getElementById("onoff");
-      if (!tFBool) {
-        tFBool = true;
-        x.innerHTML = "on";
-        z = "on";
-      } else {
-        tFBool = false;
+document.addEventListener('DOMContentLoaded', function () {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+    chrome.storage.local.get('enabled', function (result) {
 
-        x.innerHTML = "off";
-        z = "off";
-      }
-      console.log(tFBool);
+        console.log("initial status: " + result.enabled);
+        if (result.enabled != null) {
+            checkbox.checked = result.enabled;
+        }
     });
-  }
-});
-
-// need to fiugure out how to save the previos state
-chrome.tabs.onActivated.addListener((tab) => {
-  chrome.tabs.get(tab.tabId, (current_tab_info) => {
-    var z;
-    document.addEventListener("DOMContentLoaded", function () {
-      z = document.getElementById("onoff");
+  
+if(checkbox){
+    checkbox.addEventListener('click', function () {
+        console.log("current status: " + checkbox.checked);
+        chrome.storage.local.set({ 'enabled': checkbox.checked }, function () {
+            console.log("confirmed");
+        
+        });
     });
-
-    if (z) {
-      if (z.innerHTML === "on") {
-        console.log(current_tab_info.url);
-      }
-    }
-  });
+}
 });
