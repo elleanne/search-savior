b14529c1c197e2087927f59466c5eb12db5d9458
eliboss <56413692+eliboss@users.noreply.github.com>
2021-02-17 22:58:52 -0500

    Make nodes and tree (#15)

--- background.js
+++ background.js
@@ -1,30 +1,33 @@
-const chrome = require('sinon-chrome');
-window.chrome = chrome;
+const chrome = require('sinon-chrome'); // for testing only
+window.chrome = chrome; // for testing only
 
-var tFBool = false;
+var tFBool = true;
 var treeArray = [];
+var curr_parentId = null;
+var allID = [];
+var allNodes = [];
 
+class TreeNode {
+  constructor(value) {
+    this.value = value;
+    this.descendants = [];
+  }
+}
+var node = new TreeNode();
 document.addEventListener("DOMContentLoaded", function () {
-  var on = document.getElementById("checkboxid");
-  if (on) {
-    on.addEventListener("click", function () {
-      var x = document.getElementById("onoff");
-      if (!tFBool) {
-        tFBool = true;
-        x.innerHTML = "on";
-        z = "on";
-        
-      } else {
-        tFBool = false;
+  var checkbox = document.querySelector('input[type="checkbox"]');
+  chrome.storage.local.get("enabled", function (result) {
+    console.log("initial status: " + result.enabled);
+    if (result.enabled !== null && checkbox !== null) {
+      checkbox.checked = result.enabled;
+    }
+  });
 
-        x.innerHTML = "off";
-        z = "off";
-        //console.log(st.data);
-      }
-      console.log(tFBool);
-      chrome.storage.local.set({'z': z}, function() {
-        // Notify that we saved.
-          console.log(z);
+  if (checkbox) {
+    checkbox.addEventListener("click", function () {
+      console.log("current status: " + checkbox.checked);
+      chrome.storage.local.set({ enabled: checkbox.checked }, function () {
+        console.log("confirmed");
       });
     });
   }
@@ -33,17 +36,63 @@ document.addEventListener("DOMContentLoaded", function () {
 // need to fiugure out how to save the previos state
 chrome.tabs.onActivated.addListener((tab) => {
   chrome.tabs.get(tab.tabId, (current_tab_info) => {
-    var z;
-    document.addEventListener("DOMContentLoaded", function () {
-      z = document.getElementById("onoff");
-    });
-
-    if (z) {
-      if (z.innerHTML === "on") {
+    var checkbox = document.querySelector('input[type="checkbox"]');
+    chrome.storage.local.get("enabled", function (result) {
+      console.log("initial status: " + result.enabled);
+      if (result.enabled) {
         console.log(current_tab_info.url);
-        var newdata = {id: tab.tabId, parentId: 1, url: current_tab_info.url};
-        //st.data.push(newdata);
+        var newdata = {
+          id: tab.tabId,
+          parentId: curr_parentId,
+          url: current_tab_info.url,
+        };
+
+        console.log(allID + " array");
+        if (allID !== null) {
+          if (allID.includes(tab.tabId)) {
+            for(i in allNodes) {
+                if(allNodes[i].value === current_tab_info.url) {
+                    node = allNodes[i];
+                    break;
+                }
+            
+            }
+            console.log(node);
+            console.log("already there");
+            curr_parentId = tab.tabId;
+            var tempchild = [];
+            for(i in treeArray) {
+                console.log(treeArray[i].parentId);
+                if(treeArray[i].parentId === tab.tabId) {
+                    tempchild.push(treeArray[i].url);
+                }
+            }
+            console.log("tempchild" + tempchild);
+            node.descendants.push(tempchild);
+            console.log(node);
+          } else {
+            node = new TreeNode(current_tab_info.url);
+            curr_parentId = tab.tabId;
+            console.log(newdata);
+            treeArray.push(newdata);
+            console.log(treeArray);
+            allID.push(tab.tabId);
+            allNodes.push(node);
+            
+            console.log(node);
+          }
+        } else {
+          node = new TreeNode(current_tab_info.url);
+          curr_parentId = tab.tabId;
+          console.log(newdata);
+          treeArray.push(newdata);
+          console.log(treeArray);
+          allID.push(tab.tabId);
+          allNodes.push(node);
+          //node.descendants.push(newdata.url);
+          console.log(node);
+        }
       }
-    }
+    });
   });
 });
